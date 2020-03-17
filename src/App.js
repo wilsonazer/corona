5c53155e5ce964c0954aa0a1b81d50847060ef42
wilson azer <wilsonazer@hotmail.com>
2020-03-17 16:59:58 -0300

    add axios get google new api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React,{useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios');
+
 function App() {
+  const url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=br&' +
+           'category=health&'+
+           'q=corona&'+
+          'apiKey=748e2f82b67543c4b0d9a7c84a9e7db8';
+  useEffect( async ()=>{
+   const response = await  axios.get(url)
+
+   console.log(response)
+    
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        const axios = require('axios');
-var url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=br&' +
-           'category=health&'+
-           'q=corona&'+
-          'apiKey=748e2f82b67543c4b0d9a7c84a9e7db8';
-axios.get(url)
-    .then( response => {
-        console.log(response.data)
-    })
+      
+
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
