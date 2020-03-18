49718e3db69fe7e5222fe086e9d596fa294adf00
wilson azer <wilsonazer@hotmail.com>
2020-03-18 00:33:11 -0300

    add styles to app

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React,{useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React,{ Component } from 'react';
+import './App.css'
 const axios = require('axios');
 
-function App() {
-  const url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=br&' +
-           'category=health&'+
-           'q=corona&'+
-          'apiKey=748e2f82b67543c4b0d9a7c84a9e7db8';
-  useEffect( async ()=>{
-   const response = await  axios.get(url)
+const URL = 'http://newsapi.org/v2/top-headlines?' +
+                                      'country=br&' +
+                                      'category=health&'+
+                                      'q=corona&'+
+                                      'apiKey=748e2f82b67543c4b0d9a7c84a9e7db8';
 
-   console.log(response)
-    
-  },[])
+export default class App extends Component{
+  state = { 
+    news:[]
+  }
+  async componentDidMount(){
+    const res = await axios.get( URL )
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-      
+    this.setState({ news: res.data.articles})
+  }
+  render(){
+    return(
+        <section className='container'>
+          <h1>Notícias coronavírus</h1>
+          <article>
+            <ul className='list'>
+              {
+                this.state.news.map(n=>
 
-
-        </p>
-       
-      </header>
-    </div>
-  );
+                <li key={n.title} className='list-details'>
+                   {n.title}
+                   <img src={n.urlToImage} alt={n.title}/>
+                   </li>
+                )
+              }
+            </ul>
+          </article>
+        </section>
+    )
+  }
 }
-
-export default App;
